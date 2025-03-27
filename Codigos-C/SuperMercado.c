01a95066cbaef7330c145d6a27afb0282548b51c
Baumaro <botero@etrr.edu.ar>
2025-03-26 22:43:09 -0300

    super mercado

--- Codigos-C/SuperMercado.c
+++ Codigos-C/SuperMercado.c
@@ -1,80 +1,123 @@
 #include <stdio.h>
+#include <string.h>
+#define MAX_LINEA 21
 
-union flag{
+union flag
+{
 
-    struct flags{
-        unsigned int Bit0:1;
-        unsigned int Bit1:1;
-        unsigned int Bit2:1;
-        unsigned int Bit3:1;
-        unsigned int Bit4:1;
-        unsigned int Bit5:1;
-        unsigned int Bit6:1;
-        unsigned int Bit7:1;
+    struct bits
+    {
+        unsigned int Bit0 : 1;
+        unsigned int Bit1 : 1;
+        unsigned int Bit2 : 1;
+        unsigned int Bit3 : 1;
+        unsigned int Bit4 : 1;
+        unsigned int Bit5 : 1;
+        unsigned int Bit6 : 1;
+        unsigned int Bit7 : 1;
     };
 
-    char VarElec;
+    char Var;
 };
-typedef struct{
+
+typedef struct
+{
 
     char Nombre_Usuario[10];
     char Contraseña[10];
     int Rango;
 
-}Usuario_t;
+} Usuario_t;
 
-typedef struct{
+typedef struct
+{
 
     char Nombre[10];
     float Precio;
     int Codigo;
 
-}Producto_t;
-
-void Crear_Archivo(void);
+} Producto_t;
 
 void Crear_Usuario(void);
 
+void Iniciar_sesion(union flag flag_elec);
 
 int main()
 {
 
-    flag
+    union flag flag_elec;
+    flag_elec.Var = 0;
 
     printf("Bienvenido a Super Baumaro\n");
 
-
-
     printf("Desea acceder al sistema de admin? \n(1) - Si \n(0) - no");
-    scanf("%d", );
+    scanf("%d", &flag_elec.bits.bitELEC);
 
-
-
-    printf("Desea ver el catalogo de productos? \n(1) - Si \n(0) - no");
-    scanf("%d", &Var_Elec);
-
-    if (Var_Elec == 1)
-    {
-        
-    }
-    
-
-
-
+    printf("\nDesea ver el catalogo de productos? \n(1) - Si \n(0) - no");
+    scanf("%d", &flag_elec.bits.bit0);
 }
 
-void Crear_Ar
-
-void Crear_Usuarios(){
+void Crear_Usuarios()
+{
 
     Usuario_t Nuevo_Usuario;
 
-    scanf("Ingrese el nombre de usuario de la cuenta: %s", Nuevo_Usuario.Nombre_Usuario);
-    scanf("Ingrese la contraseña de la cuenta: %s", Nuevo_Usuario.Contraseña);
-    scanf("Ingrese el rango que quiere asignarle a la cuenta: %d", Nuevo_Usuario.Rango);
+    printf("\nIngrese el nombre de usuario de la cuenta: ");
+    scanf("%s", Nuevo_Usuario.Nombre_Usuario);
 
-    fopen("Base_de_usuarios");
+    printf("\nIngrese la contraseña de la cuenta: ");
+    scanf("%s", Nuevo_Usuario.Contraseña);
 
+    printf("\nIngrese el rango que quiere asignarle a la cuenta: ");
+    scanf("%d", &Nuevo_Usuario.Rango);
 
+    FILE *fp = fopen("Base_de_usuarios.csv", "a");
 
+    fprintf(fp, "Nombre Usuario,Contraseña,Rango\n");
+
+    fprintf(fp, "%s,%s,%d\n", Nuevo_Usuario.Nombre_Usuario, Nuevo_Usuario.Contraseña, Nuevo_Usuario.Rango);
+
+    fclose(fp);
+}
+
+void Iniciar_sesion(union flag flags_elec)
+{
+
+    Usuario_t Usuario_inicio;
+
+    char Linea[MAX_LINEA];
+
+    printf("\nIngrese el nombre de usuario de la cuenta que quiere iniciar sesion: ");
+    scanf("%s", Usuario_inicio.Nombre_Usuario);
+
+    printf("\nIngrese la contraseña de la cuenta que quiere iniciar sesion: ");
+    scanf("%s", Usuario_inicio.Contraseña);
+
+    printf("\nIngrese el rango que quiere asignarle a la cuenta: ");
+    scanf("%d", Usuario_inicio.Rango);
+
+    FILE *fp = fopen("Base_de_usuarios.csv", "r");
+
+    while (fgets(Linea, 10, fp))
+    {
+
+        char *NU = strtok(Linea, ",");
+        char *CS = strtok(NULL, ",");
+        char *RG = strtok(NULL, ",");
+
+        if (strcmp((NU, Usuario_inicio.Nombre_Usuario) == 0))
+        {
+
+            if (strcmp((CS, Usuario_inicio.Contraseña) == 0))
+            {
+
+                printf("se ha iniciado sesion con exito");
+            }
+            else
+            {
+
+                printf("la contraseña que ha ingresado esta mal");
+            }
+        }
+    }
 }
