b55f538260a8b08aa828d60a5025cbf4a69bbdb6
Baumaro <botero@etrr.edu.ar>
2025-05-22 17:14:41 -0300

    c

--- Sistema panol_baumaro.py/MAQUINA_ESTADO_DP.cpp
+++ Sistema panol_baumaro.py/MAQUINA_ESTADO_DP.cpp
@@ -4,62 +4,82 @@
 
 #define VALOR_ERROR -1
 
-#define DERECHA 1
-#define QUIETO 0
-#define IZQUIERDA -1
+enum movimiento
+{
+    IZQUIERDA = -1,
+    QUIETO,
+    DERECHA
+};
 
-#define ENCODER_1 6
-#define ENCODER_2 7
-#define ENCODER_OK 8
+enum encoder
+{
+    ENCODER_1 = 6,
+    ENCODER_2,
+    ENCODER_OK
+};
 
 #define DISPLAY_RS 12
 #define DISPLAY_ENABLE 11
-#define DISPLAY_D4 5
-#define DISPLAY_D5 4
-#define DISPLAY_D6 3
-#define DISPLAY_D7 2
+
+enum display
+{
+    DISPLAY_D7 = 2,
+    DISPLAY_D6,
+    DISPLAY_D5,
+    DISPLAY_D4
+};
+
+enum tiempo
+{
+    ANIOS = 1,
+    MESES,
+    DIAS,
+    HORAS,
+    MINUTOS,
+    SEGUNDOS
+};
+
+enum estados
+{
+    ESTADO_A,
+    ESTADO_B,
+    ESTADO_C,
+    ESTADO_D
+};
 
 #define COLUMNAS 16
 #define FILAS 2
 
-#define SEGUNDOS 6
-#define MINUTOS 5
-#define HORAS 4
-#define DIAS 3
-#define MESES 2
-#define ANIOS 1
+#define SEGUNDO 1000
 
 // crea el objeto lcd de tipo LiquidCrystal para representar el display 16*2
 
 LiquidCrystal lcd(DISPLAY_RS, DISPLAY_ENABLE, DISPLAY_D4, DISPLAY_D5, DISPLAY_D6, DISPLAY_D7);
 
-char tiempo[6] = {0,0,0,0,0,0};
-
-int DisplayCursor[2] = {0,0};
-
-int *i_encoder;
-
-int movimiento;
-
-int cant_loops = 0;
-
-bool tiempo_seteado = false;
-
 void Actualizar_MaquinaEstado(int *);
 
 void parpadear_texto(char texto[], int tiempo, int tiempo_parpadeo);
 
-
+void tiempo();
 
 void setup()
 {
 
-    pinmode(DISPLAY, OUTPUT);
-    pinmode(ENCODER_1, INPUT);
-    pinmode(ENCONDER_2, INPUT);
-    pinmode(ENCODER_OK, INPUT);
-    lcd.begin(COLUMNAS, FILAS);
+    pinmode(DISPLAY_D4, OUTPUT);
+    pinmode(DISPLAY_D5, OUTPUT);
+    pinmode(DISPLAY_D6, OUTPUT);
+    pinmode(DISPLAY_D7, OUTPUT);
+    pinmode(DISPLAY_RS, OUTPUT);
+    pinmode(DISPLAY_ENABLE, OUTPUT);
 
+    pinmode(ENCODER_1, INPUT);
+    pinmode(ENCODER_2, INPUT);
+    pinmode(ENCODER_OK, INPUT);
+
+    lcd.begin(COLUMNAS, FILAS);
+    static int tiempo[6] = {0, 0, 0, 0, 0, 0};
+    static int DisplayCursor[2] = {0, 0};
+    static int cant_loops = 0;
 }
 
 void loop()
@@ -71,29 +91,41 @@ void loop()
     {
         while (tiempo_seteado)
         {
-            *i_encoder = digitalread(ENCODER_1) || (digitalread(ENCODER_2) << 1);
+            int *i_encoder = digitalread(ENCODER_1) || (digitalread(ENCODER_2) << 1);
 
-            movimiento = Actualizar_MaquinaEstado(i_encoder);
+            int movimiento = Actualizar_MaquinaEstado(i_encoder);
 
-            tiempo_seteado = definir_hora(movimiento);
+            int tiempo_seteado = definir_hora(movimiento);
         }
     }
 
+    tiempo[SEGUNDOS] += 1 if (tiempo[SEGUNDOS] > 60)
+    {
+        tiempo[SEGUNDOS]-- tiempo[MINUTOS] += 1 if (tiempo[MINUTOS] > 60)
+        {
+        }
+    }
 }
 
-void definir_hora(int movimiento){
+void tiempo()
+{
+}
 
-    lcd.setCursor(DisplayCursor[0],DisplayCursor[1]);
-    lcd.print("FECHA: %d:%d:%d", tiempo[ANIOS],tiempo[MESES],tiempo[DIAS]);
+void definir_hora(int movimiento)
+{
+
+    lcd.setCursor(DisplayCursor[0], DisplayCursor[1]);
+    lcd.print("FECHA: %d:%d:%d", tiempo[ANIOS], tiempo[MESES], tiempo[DIAS]);
 
     DisplayCursor[1] = 1;
-    lcd.setCursor(DisplayCursor[0],DisplayCursor[1]);
-    lcd.print("HORA: %d:%d:%d", tiempo[HORAS],tiempo[MINUTOS],tiempo[SEGUNDOS]);
+    lcd.setCursor(DisplayCursor[0], DisplayCursor[1]);
+    lcd.print("HORA: %d:%d:%d", tiempo[HORAS], tiempo[MINUTOS], tiempo[SEGUNDOS]);
 
     DisplayCursor[0] = 15;
-    lcd.setCursor(DisplayCursor[0],DisplayCursor[1]);
+    lcd.setCursor(DisplayCursor[0], DisplayCursor[1]);
 
-    if(ENCODER_OK == 1){
+    if (1 == digital(ENCODER_1))
+    {
 
         DisplayCursor[0]--;
 
@@ -101,145 +133,141 @@ void definir_hora(int movimiento){
         {
             DisplayCursor[0] = 15;
             DisplayCursor[1] = 0;
-        }else if ((DisplayCursor[0] == -1) && (DisplayCursor[1] == 0))
+        }
+        else if ((DisplayCursor[0] == -1) && (DisplayCursor[1] == 0))
         {
             DisplayCursor[0]++;
             return true;
         }
 
-        lcd.setCursor(DisplayCursor[0],DisplayCursor[1]);
-
+        lcd.setCursor(DisplayCursor[0], DisplayCursor[1]);
     }
-    return false;
+    return false
 }
 
-void Actualizar_MaquinaEstado(int &i_encoder)
+int Actualizar_MaquinaEstado(int &i_encoder)
 {
 
     int movimiento;
 
     switch (*i_encoder)
-    case 0:
+    case ESTADO_A:
 
         int Actualizacion_encoder = digitalread(ENCODER_1) || (digitalread(ENCODER_2) << 1);
 
-        if (Actualizacion_encoder == 1)
-        {
+    if (Actualizacion_encoder == 1)
+    {
 
-            movimiento = DERECHA;
-        }
-        else if (Actualizacion_encoder == 2)
-        {
+        movimiento = DERECHA;
+    }
+    else if (Actualizacion_encoder == 2)
+    {
 
-            *i_encoder = Actualizacion_encoder;
-            movimiento = IZQUIERDA;
-        }
-        else if (Actualizacion_encoder == 0)
-        {
+        *i_encoder = Actualizacion_encoder;
+        movimiento = IZQUIERDA;
+    }
+    else if (Actualizacion_encoder == 0)
+    {
 
-            *i_encoder = Actualizacion_encoder;
-            movimiento = QUIETO;
-        }
-        else
-        {
+        *i_encoder = Actualizacion_encoder;
+        movimiento = QUIETO;
+    }
+    else
+    {
 
-            parpadear_texto("ERROR", "VALOR INVALIDO", 3000, 500);
-            exit(VALOR_ERROR);
+        parpadear_texto("ERROR", "VALOR INVALIDO", 3000, 500);
+        exit(VALOR_ERROR);
+    }
+    break;
 
-        }
+case ESTADO_B:
 
-        break;
-    case 1:
+    int Actualizacion_encoder = digitalread(ENCODER_1) || (digitalread(ENCODER_2) << 1);
 
-        int Actualizacion_encoder = digitalread(ENCODER_1) || (digitalread(ENCODER_2) << 1);
+    if (Actualizacion_encoder == 3)
+    {
 
-        if (Actualizacion_encoder == 3)
-        {
+        movimiento = DERECHA;
+    }
+    else if (Actualizacion_encoder == 0)
+    {
 
-            movimiento = DERECHA;
-        }
-        else if (Actualizacion_encoder == 0)
-        {
+        *i_encoder = Actualizacion_encoder;
+        movimiento = IZQUIERDA;
+    }
+    else if (Actualizacion_encoder == 1)
+    {
 
-            *i_encoder = Actualizacion_encoder;
-            movimiento = IZQUIERDA;
-        }
-        else if (Actualizacion_encoder == 1)
-        {
+        *i_encoder = Actualizacion_encoder;
+        movimiento = QUIETO;
+    }
+    else
+    {
 
-            *i_encoder = Actualizacion_encoder;
-            movimiento = QUIETO;
-        }
-        else
-        {
+        parpadear_texto("ERROR", "VALOR INVALIDO", 3000, 500);
+        exit(VALOR_ERROR);
+    }
 
-            parpadear_texto("ERROR", "VALOR INVALIDO", 3000, 500);
-            exit(VALOR_ERROR);
+    break;
 
-        }
+case ESTADO_C:
 
-        break;
+    int Actualizacion_encoder = digitalread(ENCODER_1) || (digitalread(ENCODER_2) << 1);
 
-    case 2:
+    if (Actualizacion_encoder == 0)
+    {
 
-        int Actualizacion_encoder = digitalread(ENCODER_1) || (digitalread(ENCODER_2) << 1);
+        movimiento = DERECHA;
+    }
+    else if (Actualizacion_encoder == 3)
+    {
 
-        if (Actualizacion_encoder == 0)
-        {
+        *i_encoder = Actualizacion_encoder;
+        movimiento = IZQUIERDA;
+    }
+    else if (Actualizacion_encoder == 2)
+    {
 
-            movimiento = DERECHA;
-        }
-        else if (Actualizacion_encoder == 3)
-        {
+        *i_encoder = Actualizacion_encoder;
+        movimiento = QUIETO;
+    }
+    else
+    {
 
-            *i_encoder = Actualizacion_encoder;
-            movimiento = IZQUIERDA;
-        }
-        else if (Actualizacion_encoder == 2)
-        {
+        parpadear_texto("ERROR", "VALOR INVALIDO", 3000, 500);
+        exit(VALOR_ERROR);
+    }
 
-            *i_encoder = Actualizacion_encoder;
-            movimiento = QUIETO;
-        }
-        else
-        {
+    break;
 
-            parpadear_texto("ERROR", "VALOR INVALIDO", 3000, 500);
-            exit(VALOR_ERROR);
+case ESTADO_D:
 
-        }
+    int Actualizacion_encoder = digitalread(ENCODER_1) || (digitalread(ENCODER_2) << 1);
 
-        break;
+    if (Actualizacion_encoder == 2)
+    {
 
-    case 3:
+        movimiento = DERECHA;
+    }
+    else if (Actualizacion_encoder == 1)
+    {
 
-        int Actualizacion_encoder = digitalread(ENCODER_1) || (digitalread(ENCODER_2) << 1);
+        *i_encoder = Actualizacion_encoder;
+        movimiento = IZQUIERDA;
+    }
+    else if (Actualizacion_encoder == 3)
+    {
 
-        if (Actualizacion_encoder == 2)
-        {
+        *i_encoder = Actualizacion_encoder;
+        movimiento = QUIETO;
+    }
+    else
+    {
 
-            movimiento = DERECHA;
-        }
-        else if (Actualizacion_encoder == 1)
-        {
-
-            *i_encoder = Actualizacion_encoder;
-            movimiento = IZQUIERDA;
-        }
-        else if (Actualizacion_encoder == 3)
-        {
-
-            *i_encoder = Actualizacion_encoder;
-            movimiento = QUIETO;
-        }
-        else
-        {
-
-            parpadear_texto("ERROR", "VALOR INVALIDO", 3000, 500);
-            exit(VALOR_ERROR);
-
-        }
-        break;
+        parpadear_texto("ERROR", "VALOR INVALIDO", 3000, 500);
+        exit(VALOR_ERROR);
+    }
+    break;
 
     return movimiento;
 }
@@ -252,21 +280,18 @@ void parpadear_texto(char texto_1[], char texto_2[], int tiempo, int tiempo_parp
     {
         int i++;
 
-        
-
         if ((i % 2) == 0)
         {
 
             lcd.print(texto_1);
-            lcd.setCursor(0,1);
+            lcd.setCursor(0, 1);
             lcd.print(texto_2);
-
-        }else{
+        }
+        else
+        {
 
             lcd.clear();
-
         }
         delay(tiempo_parpadeo)
     }
-    
 }
